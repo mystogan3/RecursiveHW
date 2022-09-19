d6a2078d608232c80187b49161c6ef1580e9d233
PoliakovAY <myatogan3@yandex.ru>
2022-09-19 14:01:13 +0300

    HW about recursive

---
+++ Task66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int GetSum (int sum, int M, int N)
+{
+    while (M <= N)
+    {
+        sum+=M;
+        GetSum(sum, M+=1,N);
+    } 
+    return sum;
+}
+int sum = 0;
+Console.Write("M=1; N=8 -->");
+Console.WriteLine(GetSum(sum,1,8));
---
+++ Task68/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 3, n = 2 -> A(m,n) = 29
+
+int GetResult( int m, int n) 
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return GetResult(m - 1, 1);
+    return GetResult(m - 1, GetResult(m, n - 1));
+}
+
+Console.Write("m = 3, n = 2 --> ");
+Console.WriteLine($"A(3,2) = {GetResult(3,2)}");
+
